1d560417aa3e5755b15cb99958d17777c59400b6
Lucas Vihuchi <lucasvihuchi@gmail.com>
2022-07-19 08:46:04 -0300

    Reimplement cpf validation

--- src/main/kotlin/com/newbank/utils/Validators.kt
+++ src/main/kotlin/com/newbank/utils/Validators.kt
@@ -1,34 +1,50 @@
 package com.newbank.utils
 
-import com.newbank.exceptions.IncorrectInfoException
-
 class Validators {
 
     companion object {
         fun validateCpf(cpf: String?): Boolean {
-            return (cpf is String && cpf.matches("^[0-9]{11}\$".toRegex()))
-        }
-
-        fun validateCpf2(cpf: String?): Boolean {
-            if (cpf !is String || cpf.length != 11 || cpf is Num) {
+            if (cpf !is String || cpf.matches("^[0-9]{11}\$".toRegex())) {
                 return false
             }
 
             val digits: Array<Int> = Array(11, { i -> cpf[i].digitToInt() })
             var areCharactersEqual: Boolean = true
             var index = 0
-            for (index in 0..10) {
+            for (index in 0 until 11) {
                 if (index > 0) {
-                    if (digits[index] != digits[index-1]) {
+                    if (digits[index] != digits[index - 1]) {
                         areCharactersEqual = false
                     }
                 }
             }
-            if(areCharactersEqual) {
+            if (areCharactersEqual) {
                 return false
             }
 
-            TODO ("Terminar isso")
+            var firstDigitSumVerification: Int = 0
+            for (index in 0 until 9) {
+                firstDigitSumVerification += digits[index] * (10 - index)
+            }
+            firstDigitSumVerification *= 10
+            if (
+                digits[9] != (firstDigitSumVerification % 11) &&
+                !((digits[9] == 0) && (firstDigitSumVerification % 11) == 10)
+            ) {
+                return false
+            }
+
+            var secondDigitSumVerification: Int = 0
+            for (index in 0 until 10) {
+                secondDigitSumVerification += digits[index] * (11 - index)
+            }
+            secondDigitSumVerification *= 10
+            if (
+                digits[10] != (secondDigitSumVerification % 11) &&
+                !((digits[10] == 0) && (secondDigitSumVerification % 11) == 10)
+            ) {
+                return false
+            }
 
             return true
         }
